409177a43c4798c2152cc24c76f0d5cad17d1dfc
Daniel Choo <danielchoo@Daniels-MacBook-Pro-68.local>
2022-11-06 12:48:41 -0500

    lsp

--- init.lua
+++ init.lua
@@ -158,8 +158,8 @@ cmp.setup {
         end,
     },
     mapping = cmp.mapping.preset.insert({
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<CR>'] = cmp.mapping.confirm({
             select = true,
@@ -236,7 +236,6 @@ lualine.setup {
                 mode = 0,
             }
         },
-        lualine_c = {},
     },
     inactive_sections = {
         lualine_a = {},
